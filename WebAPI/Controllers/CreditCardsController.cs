a8fdd4cb30e068af9c432c6916426e534782f3f6
ABarcin <74905523+ABarcin@users.noreply.github.com>
2021-04-11 18:29:29 +0300

    refactoring

---
+++ WebAPI/Controllers/CreditCardsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entitites.Concrete;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditCardsController : ControllerBase
+    {
+        IWebHostEnvironment _webHostEnvironment;
+        ICreditCardCheckService _creditCardCheckService;
+
+        public CreditCardsController(ICreditCardCheckService creditCardCheckService, IWebHostEnvironment webHostEnvironment)
+        {
+            _creditCardCheckService = creditCardCheckService;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost("check")]
+        public bool Check(CreditCard creditCard)
+        {
+            bool result = _creditCardCheckService.CheckCreditCard(creditCard);
+            return result;
+        }
+    }
+}
